7cfeae2eebf5d88354475c425771686e94528896
TeamVery <hellow1211other@gmail.com>
2020-09-15 13:33:16 +0900

    Add Firework Execute

--- src/main/java/com/teamvery/lobbya/cmd/settings.java
+++ src/main/java/com/teamvery/lobbya/cmd/settings.java
@@ -2,11 +2,16 @@ package com.teamvery.lobbya.cmd;
 
 import com.teamvery.lobbya.config;
 import org.bukkit.Bukkit;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
 
 public class settings implements CommandExecutor {
@@ -30,6 +35,19 @@ public class settings implements CommandExecutor {
                     String cmd = "gamerule doDaylightCycle true";
                     Bukkit.dispatchCommand(console, cmd);
                 }
+            }
+            if (args[0].equalsIgnoreCase("debug")) {
+                Firework firework = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
+                FireworkMeta meta = firework.getFireworkMeta();
+                FireworkEffect.Builder builder = FireworkEffect.builder();
+                builder.withTrail().withFlicker().withColor(Color.GREEN, Color.WHITE, Color.YELLOW, Color.BLUE,
+                        Color.FUCHSIA, Color.PURPLE, Color.MAROON, Color.LIME, Color.ORANGE)
+                        .with(FireworkEffect.Type.BALL_LARGE);
+                meta.addEffect(FireworkEffect.builder().withColor(Color.AQUA).trail(true).build());
+                meta.setPower(1);
+                firework.setFireworkMeta(meta);
+
+                player.sendMessage("디버그 실행됨.");
             }
         } else {
             player.sendMessage("§7---------- §3LobbyA 플러그인 명령어 §7----------");
--- src/main/java/com/teamvery/lobbya/event/joinquitevent.java
+++ src/main/java/com/teamvery/lobbya/event/joinquitevent.java
@@ -1,10 +1,15 @@
 package com.teamvery.lobbya.event;
 
+import org.bukkit.*;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
@@ -12,19 +17,63 @@ import static com.teamvery.lobbya.config.config;
 
 public class joinquitevent implements @NotNull Listener {
 
+    ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+
     @EventHandler
     void onPlayerJoin(PlayerJoinEvent e) {
         Player player = e.getPlayer();
         boolean hsb = player.hasPlayedBefore();
 
         if(!hsb) {
-            e.setJoinMessage(Objects.requireNonNull(config.getString("첫접속.입장"))
-                    .replace("&", "§")
-                    .replace("%player%", player.getName()));
+            if (config.getBoolean("첫접속.소리.활성화")) {
+                player.playSound(player.getLocation(), Sound.valueOf(config.getString("첫접속.소리.사운드")), SoundCategory.MASTER, 1, 1);
+            }
+            if (config.getBoolean("첫접속.명령어(콘솔).활성화")) {
+                String cmd = config.getString("첫접속.명령어(콘솔).명령어");
+                Bukkit.dispatchCommand(console, Objects.requireNonNull(cmd));
+            }
+            if (config.getBoolean("첫접속.폭죽")) {
+                Firework firework = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
+                FireworkMeta meta = firework.getFireworkMeta();
+                FireworkEffect.Builder builder = FireworkEffect.builder();
+                builder.withTrail().withFlicker().withColor(Color.GREEN, Color.WHITE, Color.YELLOW, Color.BLUE,
+                        Color.FUCHSIA, Color.PURPLE, Color.MAROON, Color.LIME, Color.ORANGE)
+                        .with(FireworkEffect.Type.BALL_LARGE);
+                meta.addEffect(builder.build());
+                meta.setPower(1);
+                firework.setFireworkMeta(meta);
+                firework.detonate();
+            }
+            if (!(config.getString("첫접속.입장") == null)) {
+                e.setJoinMessage(Objects.requireNonNull(config.getString("첫접속.입장"))
+                        .replace("&", "§")
+                        .replace("%player%", player.getName()));
+            }
         } else {
-            e.setJoinMessage(Objects.requireNonNull(config.getString("재접속.입장"))
-                    .replace("&", "§")
-                    .replace("%player%", player.getName()));
+            if (config.getBoolean("재접속.소리.활성화")) {
+                player.playSound(player.getLocation(), Sound.valueOf(config.getString("재접속.소리.사운드")), SoundCategory.MASTER, 1, 1);
+            }
+            if (config.getBoolean("재접속.명령어(콘솔).활성화")) {
+                String cmd = config.getString("재접속.명령어(콘솔).명령어");
+                Bukkit.dispatchCommand(console, Objects.requireNonNull(cmd));
+            }
+            if (config.getBoolean("재접속.폭죽")) {
+                Firework firework = (Firework) player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREWORK);
+                FireworkMeta meta = firework.getFireworkMeta();
+                FireworkEffect.Builder builder = FireworkEffect.builder();
+                builder.withTrail().withFlicker().withColor(Color.GREEN, Color.WHITE, Color.YELLOW, Color.BLUE,
+                        Color.FUCHSIA, Color.PURPLE, Color.MAROON, Color.LIME, Color.ORANGE)
+                        .with(FireworkEffect.Type.BALL_LARGE);
+                meta.addEffect(builder.build());
+                meta.setPower(1);
+                firework.setFireworkMeta(meta);
+                firework.detonate();
+            }
+            if (!(config.getString("재접속.입장") == null)) {
+                e.setJoinMessage(Objects.requireNonNull(config.getString("재접속.입장"))
+                        .replace("&", "§")
+                        .replace("%player%", player.getName()));
+            }
         }
         if (config.getBoolean("초기화")) {
             player.getInventory().clear();
